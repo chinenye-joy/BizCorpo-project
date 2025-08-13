f27a1c246b17c56daafa5b384fa61fa9b5b5e994
chinenye-joy <anyimjoy01@gmail.com>
2025-08-13 03:32:38 +0100

    Add initial CSS styles for layout, typography, and responsiveness

---
+++ main.js
@@ -0,0 +1,23 @@
+const openMenu = document.getElementById("openMenu");
+const closeMenu = document.getElementById("closeMenu");
+const navLinks = document.querySelector(".nav-links");
+const btns = document.querySelector(".buttons");
+// const socialIcons = document.querySelector('.social-icons');
+
+openMenu.addEventListener("click", () => {
+  navLinks.classList.add("open");
+  //   socialIcons.classList.add('open');
+  btns.classList.add("open");
+
+  openMenu.style.display = "none";
+  closeMenu.style.display = "block";
+});
+
+closeMenu.addEventListener("click", () => {
+  navLinks.classList.remove("open");
+  //   socialIcons.classList.remove('open');
+  btns.classList.remove("open");
+
+  openMenu.style.display = "block";
+  closeMenu.style.display = "none";
+});
