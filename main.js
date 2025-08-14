4189973a5fa5f6f962af37362a2268b84f3caada
chinenye-joy <anyimjoy01@gmail.com>
2025-08-14 04:44:13 +0100

    Integrate AOS for animations and update CSS for improved layout and responsiveness

--- main.js
+++ main.js
@@ -22,20 +22,7 @@ closeMenu.addEventListener("click", () => {
   closeMenu.style.display = "none";
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-  // Add AOS attribute to the elements you want
-  document.querySelectorAll(' section').forEach(section => {
-    section.setAttribute('data-aos', 'fade-up'); // change to any effect you like
-  });
 
-  AOS.init({
-    duration: 400, // fast animation
-    once: true     // run only once
-  });
-
-  // Force AOS to detect sections
-  AOS.refresh();
-});
  
 
 
