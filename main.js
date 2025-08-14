950c0adaa0fe76ce1e1ed1b4ad25844cab23f22f
chinenye-joy <anyimjoy01@gmail.com>
2025-08-14 04:26:32 +0100

    Enhance layout and interactivity: add AOS for animations, update CSS for responsiveness, and improve button styles

--- main.js
+++ main.js
@@ -21,3 +21,24 @@ closeMenu.addEventListener("click", () => {
   openMenu.style.display = "block";
   closeMenu.style.display = "none";
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Add AOS attribute to the elements you want
+  document.querySelectorAll(' section').forEach(section => {
+    section.setAttribute('data-aos', 'fade-up'); // change to any effect you like
+  });
+
+  AOS.init({
+    duration: 400, // fast animation
+    once: true     // run only once
+  });
+
+  // Force AOS to detect sections
+  AOS.refresh();
+});
+ 
+
+
+
+
+
